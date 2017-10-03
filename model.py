aaad0d0914bcc2c2364081f5486fb1ee55dc8753
gray-stanton <gray.stanton@hartman-group.com>
2017-10-03 14:57:45 -0700

    Finangles train into actually working, fixes dynamic_rnn state issue, very rough

--- model.py
+++ model.py
@@ -62,14 +62,16 @@ def build_main_graph(params):
     with tf.name_scope('unroll'):
         #outputs:[batch-size, seq-length, hidden-layer-sizes[-1]]
         #final_states = (hidden-layer-sizes[0], hidden-layer-sizes[1], ...)
-        print(onehot_input.shape)
-        print(len(init_hidden_states))
-        print(init_hidden_states[0].shape)
         outputs, final_states = tf.nn.dynamic_rnn(multicell,
                                                  onehot_input,
                                                  initial_state=init_hidden_states,
                                                  dtype=tf.float16,
                                                  time_major=False)
+        final_state_stack = tf.stack(
+            list(final_states), 
+            axis = 0,
+            name ='stack_states'
+        )
     with tf.name_scope('output'):
         logit_W = tf.get_variable('logit_W',
                                 shape=[params['hidden-layer-sizes'][-1],
@@ -80,8 +82,9 @@ def build_main_graph(params):
         logit_b = tf.get_variable('logit_b', 
                                   shape=params['char-map-size'],
                                   dtype=tf.float16)
-        #long_output [batch-size * seq-length, char-map-size]
-        long_output = tf.reshape(outputs, [-1, params['char-map-size']], 
+        #long_output [batch-size * seq-length, hidden-layer-sizes[-1]]
+        long_output = tf.reshape(outputs,
+                                 [-1, params['hidden-layer-sizes'][-1]], 
                                     name='reshaped_output')
         long_logits = tf.matmul(long_output, logit_W) + logit_b
         logits = tf.reshape(long_logits, [-1, params['seq-length'],
@@ -130,10 +133,16 @@ def build_main_graph(params):
         
         #Wrap up and hold state
         summaries = tf.summary.merge_all()
+        tf.add_to_collection('input_placeholders', char_input)
+        tf.add_to_collection('input_placeholders', char_label)
+        for i in range(0, len(init_hidden_states)):
+            tf.add_to_collection('input_placeholders', init_hidden_states[i])
+
         tf.add_to_collection('training_operations', train_step)
         tf.add_to_collection('summary_operations', summaries)
-        tf.add_to_collection('states', final_states)
-        tf.add_to_collection('states', init_hidden_states)
+        tf.add_to_collection('summary_operations', mean_loss)
+        tf.add_to_collection('states', final_state_stack)
+        #tf.add_to_collection('states', init_hidden_states)
         tf.add_to_collection('outputs', char_probs)
         init_op = tf.global_variables_initializer()
         tf.add_to_collection('init_ops', init_op)
@@ -160,48 +169,66 @@ def create(model_dir, params, model_name=None):
 
 
 
-def get_init_state(init, batch_size):
+def get_init_state(n_states, init, batch_size, num_cells):
     if init == 'ZERO':
-        return (tf.nn.rnn_cell.MultiRNNCell.zero_state(batch_size,
-                                                      dtype = tf.float16))
+        return [ np.zeros(dtype = np.float16, shape = (batch_size, num_cells[i]))
+                for i in range(0, n_states) ]
+        
 
-
-def train(model_dir, train_params, train_config, batch_iterator ,model_name=None):
+def train(model_dir, train_config, batch_iterator ,model_name=None):
+    print(train_config)
     if model_name is None:
         model_name = 'my_model'
-        with tf.Session() as sess:
-            try:
-                new_saver = tf.train.import_meta_graph(
-                    model_dir + model_name + '.meta'
-                    )
-            except FileNotFoundError:
-                print('Can not find meta graph')
-            new_saver.restore(sess, tf.train.latest_checkpoint(model_dir))
+    with tf.Session() as sess:
+        try:
+            new_saver = tf.train.import_meta_graph(
+                model_dir + model_name + '.meta'
+                )
+        except FileNotFoundError:
+            print('Can not find meta graph')
+        new_saver.restore(sess, tf.train.latest_checkpoint(model_dir))
+        print('restored!')
 
-            train_op = tf.get_collection('training_operations')[0]
-            summary_op = tf.get_collection('summary_operations')[0]
-            state_op = tf.get_collection('states')[0]
-            n_states = len(tf.get_collection('states')[1])
-            states = None
-            step = tf.train.get_global_step(sess, 'global_step_tensor')
-            for batch, label in batch_iterator:
-                if state is None:
-                    states = get_init_state(
-                        n_states,
-                        train_config['state-init'], batch.shape[0]
-                        )
-                feed_dict = {'char_input' : batch, 
-                             'label_input' : label,
-                            }
-                for i in range(0, n_states):
-                    feed_dict['init_hidden_states_' + str(i)] = states[i]
+        train_op = tf.get_collection('training_operations')[0]
+        summary_op = tf.get_collection('summary_operations')[0]
+        train_writer = tf.summary.FileWriter(model_dir + '/train_logs',
+                                             sess.graph)
 
-                _, _, states = sess.run([train_op, summary_op, state_op])
-                step += 1
-                if set % train_config['report-freq']:
-                    print(step)
-            new_saver.save(sess, model_dir + model_name, global_step = step)
+        input_placeholders = tf.get_collection('input_placeholders')
     
+        print(input_placeholders)
+        state_stack = tf.get_collection('states')[0]
+        n_states = int(state_stack.shape[0])
+        states = None
+        step = 0 
+        for batch, label in batch_iterator:
+            if states is None:
+                states = get_init_state(
+                    n_states,
+                    train_config['state-init'], batch.shape[0],
+                    num_cells = train_config['shapes'] #FIXME info leak
+                    )
+
+
+            feed_dict = {input_placeholders[0] : batch, # char_input
+                         input_placeholders[1] : label, # char_label
+                        }
+
+            for i in range(2, n_states + 2):
+                feed_dict[input_placeholders[i]] = states[i-2]
+
+            _, summary, state_stack_output = sess.run(
+                [train_op, summary_op, state_stack], feed_dict
+            )
+            states = [state_stack_output[i] for  i in range(0, state_stack.shape[0])]
+            step += 1
+            if step % train_config['report-freq']:
+                print(step)
+                train_writer.add_summary(summary, step)
+
+
+        new_saver.save(sess, model_dir + model_name, global_step = step)
+
 
 def evaluate():
     pass
@@ -239,12 +266,12 @@ def generate(model_dir, seed_text, gen_length, generate_config, model_name=None)
             for i in range(0, gen_length):
                 if state is None:
                     state = get_init_state( generate_config['state-init'], 1) 
-                feed_dict = {'char_input' : prompt,
-                             'label_input' : np.zeroes(
+                    feed_dict = {'char_input:0' : prompt,
+                                 'label_input:0' : np.zeroes(
                                  (1, seq_length)),
                              'init_hidden_state' : state
                             }
-                probs, state = sess.run([char_probs, state_op])
+                probs, state = sess.run([char_probs, state_stack_op])
                 last_char_probs = probs[1, seq_length, :]
                 next_char = generate_config['inv-char-map'][ softmax_sample(
                     last_char_probs, generate_config['temperature']
